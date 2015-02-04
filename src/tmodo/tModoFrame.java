86caf90500a161adc16505fdb45f66b1573dd6bf
time2git <time2rfc@gmail.com>
2015-02-04 17:45:50 +0300

    add src file

---
+++ src/tmodo/tModoFrame.java
@@ -0,0 +1,182 @@
+package tmodo;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JSlider;
+import javax.swing.Timer;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+/**
+ * @author alex
+ */
+public class tModoFrame extends javax.swing.JFrame {
+
+    public tModoFrame() {
+        initComponents();
+        stopButtonActionPerformed(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        timeLabel = new javax.swing.JLabel();
+        timeSlider = new javax.swing.JSlider();
+        startButton = new javax.swing.JButton();
+        stopButton = new javax.swing.JButton();
+        pauseButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAlwaysOnTop(true);
+        setResizable(false);
+
+        timeLabel.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        timeLabel.setText("30");
+
+        timeSlider.setMaximum(60);
+        timeSlider.setMinimum(1);
+        timeSlider.setMinorTickSpacing(10);
+        timeSlider.setPaintLabels(true);
+        timeSlider.setValue(30);
+        timeSlider.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                timeSliderStateChanged(evt);
+            }
+        });
+
+        startButton.setText("start");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        stopButton.setText("stop");
+        stopButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                stopButtonActionPerformed(evt);
+            }
+        });
+
+        pauseButton.setText("pause");
+        pauseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pauseButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(timeSlider, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(stopButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(pauseButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(startButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(timeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 47, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(timeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(timeSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(startButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(pauseButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(stopButton)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
+        startButton.setEnabled(false);
+        pauseButton.setEnabled(true);
+        stopButton.setEnabled(true);
+        timeSlider.setEnabled(false);
+        
+        timeIterator = timeSlider.getValue() * 60 ;
+        startTimer() ;
+
+    }//GEN-LAST:event_startButtonActionPerformed
+
+    private void pauseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pauseButtonActionPerformed
+        stopButton.setEnabled(!stopButton.isEnabled());
+        if(timer.isRunning()){
+            timer.stop();
+        }else{
+            timer.start() ;
+        }
+    }//GEN-LAST:event_pauseButtonActionPerformed
+        
+    private void stopButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_stopButtonActionPerformed
+        startButton.setEnabled(true);
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        timeSlider.setEnabled(true);
+        timeSlider.setVisible(true);
+        timeSlider.setEnabled(true);
+        
+        if( timer != null){
+            timer.stop();
+        }
+        timeIterator = 0 ;
+        timeLabel.setText(Integer.valueOf(timeSlider.getValue()).toString());
+
+    }//GEN-LAST:event_stopButtonActionPerformed
+
+    private void timeSliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_timeSliderStateChanged
+        JSlider slider = (JSlider) evt.getSource() ;
+        timeLabel.setText(Integer.valueOf(slider.getValue()).toString());
+    }//GEN-LAST:event_timeSliderStateChanged
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton pauseButton;
+    private javax.swing.JButton startButton;
+    private javax.swing.JButton stopButton;
+    private javax.swing.JLabel timeLabel;
+    private javax.swing.JSlider timeSlider;
+    // End of variables declaration//GEN-END:variables
+
+    private Timer timer ;
+
+    private void startTimer() {
+        timer = new Timer(1000, timerAL ) ;
+        timer.start(); 
+    }
+    
+    private static int timeIterator = 0 ;
+    private ActionListener timerAL = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if( timeIterator --  > 0 ){
+                timeLabel.setText(Integer.valueOf(timeIterator).toString());
+            }else{
+//                java.awt.EventQueue.invokeLater(() -> {
+//                    StopTimeFrame s = new StopTimeFrame() ;
+//                    s.setVisible(true);
+//                    s.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+//                });
+                
+                stopButtonActionPerformed(null);
+            }
+        }
+    };
+}
